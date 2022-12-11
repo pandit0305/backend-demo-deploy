fd9c9993117fd37b0a5aa7dcefda09ae40462fb5
pandit0305 <ptgangadhar.may3@gmail.com>
2022-12-11 12:45:24 +0530

    initial commits on demo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require("cors");
 require("dotenv").config();
-const PORT = process.env.PORT || 8080
+
 
 const {connection} = require("./config/db");
 const {userRouter} = require("./router/user.route");
@@ -18,7 +18,7 @@ app.use("/", userRouter);
 app.use(authenticate);
 app.use("/notes", noteRouter);
 
-app.listen(PORT, async ()=>{
+app.listen(process.env.PORT, async ()=>{
     try{
         await connection;
         console.log("connection successful!")
