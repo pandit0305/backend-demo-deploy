79887a38e4383b78fcf47fa53b3b1ca29c48445c
pandit0305 <ptgangadhar.may3@gmail.com>
2022-12-11 15:00:26 +0530

    initial commits on demo 123

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:3000"
+    origin:"https://todo-app-omega-rose.vercel.app"
 }))
 app.use("/", userRouter);
 
